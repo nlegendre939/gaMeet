174d956f63a95e85aac57290c4084988c10e0485
ameowbidik <aised@hotmail.fr>
2022-01-07 17:39:54 +0100

    debutform

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,37 +2,52 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Event;
+use App\Form\EventType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EventController extends AbstractController
 {
     #[Route('/event', name: 'event_')]
+
     public function form(Request $request, Event $event = null): Response
     {
+
+        // Bloc l'acces à la page si l'utilisateur n'est pas identifié
+
+
+        //dd($this->getUser());
+
+
         if($event){
             $isNew = false;
         }else{
             $event = new Event();
-            $event->setOwner($this->getUser());
+            $event->addUser($this->getUser());
             $isNew = true;
         }
         $form = $this->createForm(EventType::class, $event);
         
         $form->handleRequest($request);
+
         if($form->isSubmitted() && $form->isValid()){
-            $this->mediaService->handleEvent($event);
-            $this->em->persist($event);
+            // TODO - Remplacer par l'utilisateur connecté
+            $user = $this->em->getRepository(User::class)->find(1);
+            $ad->setUser($user);
+
+            $this->em->persist($ad);
             $this->em->flush();
 
-            $message = sprintf('Votre événement à bien été %s', $isNew ? 'créé' : 'modifié');
-            $this->addFlash('notice', $message);
+            $this->addFlash('notice', 'Votre événement à été créé');
+
             return $this->redirectToRoute('event_show', [
                 'id' => $event->getId(),
             ]);
         }
-
+        
         return $this->render('event/form.html.twig', [
             'form' => $form->createView(),
             'isNew' => $isNew
--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,22 +3,54 @@
 namespace App\Form;
 
 use App\Entity\Event;
+use App\Form\EventType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('picture')
-            ->add('description')
-            ->add('start_at')
-            ->add('endAt')
-            ->add('team')
-            ->add('user')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'événement',
+            ])
+            ->add('description', null, [
+                'attr' => [
+                    'rows' => 5,
+                ]
+            ])
+            ->add('picture', FileType::class, [
+                'label' => 'Importer une image'
+            ])
+            ->add('startAt', null, [
+                'label' => 'Date de début',
+                'date_widget' => 'single_text',
+                'time_widget' => 'single_text',
+                'attr' => [
+                    'class' => 'datetime-widget',
+                ]
+            ])
+            ->add('endAt', null, [
+                'label' => 'Date de fin',
+                'date_widget' => 'single_text',
+                'time_widget' => 'single_text',
+                'attr' => [
+                    'class' => 'datetime-widget',
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'button',
+                ]
+            ])
         ;
     }
 
@@ -26,6 +58,9 @@ class EventType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Event::class,
+            'attr' => [
+                'novalidate' => 'novalidate'
+            ]
         ]);
     }
 }
