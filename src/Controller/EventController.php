b97f2de81c0a9737f41b735cae9a80139622db27
ameowbidik <aised@hotmail.fr>
2022-01-07 12:44:59 +0100

    --

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+*namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,11 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
-    #[Route('/event', name: 'event')]
-    public function index(): Response
+    #[Route('/event', name: 'event_')]
+    public function form(Request $request, Event $event = null): Response
     {
-        return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
-        ]);
-    }
+        if($event){
+            $isNew = false;
+        }else{
+            $event = new Event();
+            $event->setOwner($this->getUser());
+            $isNew = true;
+        }
+        $form = $this->createForm(EventType::class, $event);
+        
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->mediaService->handleEvent($event);
+            $this->em->persist($event);
+            $this->em->flush();
+
+            $message = sprintf('Votre événement à bien été %s', $isNew ? 'créé' : 'modifié');
+            $this->addFlash('notice', $message);
+            return $this->redirectToRoute('event_show', [
+                'id' => $event->getId(),
+            ]);
+        }
 }
+return $this->render('event/form.html.twig', [
+    'form' => $form->createView(),
+    'isNew' => $isNew
+]);
+}
+
---
+++ src/Form/EventType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('picture')
+            ->add('description')
+            ->add('start_at')
+            ->add('endAt')
+            ->add('team')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
